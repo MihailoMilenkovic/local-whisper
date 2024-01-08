bcadd16f70d7d429c55c949e12412a1b0248eade
Mihailo Milenkovic <mihailo.milenkovic4@gmail.com>
2024-01-08 03:05:31 +0100

    cleanup

--- app/speech_to_text_flutter/lib/main.dart
+++ app/speech_to_text_flutter/lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-import 'config.dart'
+import 'config.dart';
 
 void main() {
   runApp(const MyApp());
