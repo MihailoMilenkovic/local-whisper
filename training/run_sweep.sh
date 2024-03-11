2d7790a50d65df4e05a6c0d081e3d8087675fcc6
Mihailo Milenkovic <mmilenkovic@groq.com>
2024-03-11 02:31:25 -0700

    add sweep to train and eval multiple configs

--- training/run_sweep.sh
+++ training/run_sweep.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Make script exit if a command fails
+set -Eeuo pipefail
 
-model_sizes= ("tiny" "small" "medium" "large")
-use_lora= ("true" "false")
+model_sizes=("tiny" "small" "medium" "large")
+use_lora=("true" "false")
+eval_results="eval_results.txt"
+for size in "${model_sizes[@]}"; do
+    ./run_eval.sh --model-size "$size" --base-model true >> $eval_results
+    for lora in "${use_lora[@]}"; do
+        ./run_training.sh --model-size "$size" --use-lora "$lora"
+        ./run_eval.sh --model-size "$size" --use-lora "$lora"  --base-model false >> $eval_results
+    done
+done
